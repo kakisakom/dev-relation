070d92c262c3b7624290975c0b491053890d2c87
kakisakom <68311774+kakisakom@users.noreply.github.com>
2021-02-14 10:56:35 +0900

    基本コードの完成

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,17 +1,24 @@
 class StaffMember < ApplicationRecord
+  #include EmailHolder
+  include PersonalNameHolder
+  include PasswordHolder
+  
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
-
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
+  validates :start_date, presence: true, date: {
+    after_or_equal_to: Date.new(2000, 1, 1),
+    before: -> (obj) { 1.year.from_now.to_date },
+    allow_blank: true
+  }
   
-  def active?
+  validates :end_date, date: {
+    after: :start_date,
+    before: -> (obj) { 1.year.from_now.to_date },
+    allow_blank: true
+  }
+
+    def active?
     !suspended? && start_date <= Date.today &&
       (end_date.nil? || end_date > Date.today)
-  end
+    end
 end
