8b6828c42d0c1f56631dd7e5a3736d8ceac5ca60
kakisakom <68311774+kakisakom@users.noreply.github.com>
2021-06-20 19:11:04 +0900

    許可ホストにdev-relation0619を追加

--- config/initializers/blocked_hosts.rb
+++ config/initializers/blocked_hosts.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
-  config.hosts = nil
+  #config.hosts = nil
   config.hosts << "localhost"
   config.hosts << "example.com"
   config.hosts << "baukis2.example.com"
+  config.hosts << "dev-relation0619.herokuapp.com"
  end
