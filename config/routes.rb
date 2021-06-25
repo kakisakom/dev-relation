4a58df1f60f4d14552b57e08b95e0cd2872e0ea0
kakisakom <68311774+kakisakom@users.noreply.github.com>
2021-06-25 09:01:54 +0900

    routeの解除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-	config = Rails.application.config.baukis2
+	#config = Rails.application.config.baukis2
 
-	constraints host: config[:staff][:host] do
+	#constraints host: config[:staff][:host] do
 		namespace:staff, path: config[:staff][:path] do
 	   root "top#index"
 		 get "login" => "sessions#new", as: :login
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
 		 resource :password, only: [ :show, :edit, :update ]
 		 resources :customers
 	 end
-	end
+	#end
 	
-	constraints host: config[:admin][:host] do
+	#constraints host: config[:admin][:host] do
 		namespace:admin, path: config[:admin][:path] do
 			root "top#index"
 			get "login" => "sessions#new", as: :login
@@ -24,11 +24,11 @@ Rails.application.routes.draw do
 		end
 	end
 	
-	constraints host: config[:customer][:host] do
+	#constraints host: config[:customer][:host] do
 		namespace:customer, path: config[:customer][:path] do
 		 root "top#index"
 		 get "login" => "sessions#new", as: :login
 		 resource :session, only: [:create, :destroy]
 		end
-  end
+  #end
 end
